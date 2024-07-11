7e93aeb8448e041a810730d3f0230d9a9916cec2
Saifur00 <saifurbgc@gmail.com>
2024-07-11 15:44:01 +0600

    Math operations basic

---
+++ math.py
@@ -0,0 +1,16 @@
+a = 20
+b = 12
+
+print("Addition of two digits:", a+b)
+
+print("Subtraction of two digits:", a-b)
+
+print("Multiplication of two digits:", a*b)
+
+print("Division of two digits:", a/b)
+
+print("Modulus of two digits:", a%b)
+
+print("Floor Division of two digits:", a//b)
+
+print("Exponential of two digits:", a**b)
